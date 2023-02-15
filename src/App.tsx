ae581ef16cb264b2d5eb48911ec4f5af98356359
MoinMarcell <marcell.dechant@protonmail.com>
2023-02-15 19:25:29 +0100

    Add search & conditional rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {Character} from "./model/Character";
 import Characters from "./characters.json"
 import CharacterGallery from "./component/CharacterGallery";
+import SearchCharacter from "./component/SearchCharacter";
 
 function App() {
 
     const characters: Character[] = Characters;
+    const [searchText, setSearchText] = useState<string>("");
+
+    function handleSearchText(searchText: string) {
+        setSearchText(searchText);
+    }
+
+    const filteredCharacters = characters.filter((character) =>
+        character
+            .name
+            .toLowerCase()
+            .includes(searchText.toLowerCase())
+        ||
+        character
+            .status
+            .toLowerCase()
+            .includes(searchText.toLowerCase())
+    );
 
     return (
         <div className="App">
             <h1>The one and only Rick and Morty Gallery</h1>
-            <CharacterGallery characters={characters}/>
+            Search: <SearchCharacter searchText={handleSearchText}/>
+            {
+                filteredCharacters.length > 0 ?
+                    <CharacterGallery characters={filteredCharacters}/> :
+                    <div>
+                        <p>No Characters found :(</p>
+                        <img
+                            src={"https://media0.giphy.com/media/uB6zoKcZUMgKY/giphy.gif?cid=ecf05e4764ewl4o8fiu1b5cmtiz09yfpgqg6dzt2da70dp8c&rid=giphy.gif&ct=g"}
+                            alt={"Cry me a river"}
+                        />
+                    </div>
+            }
+
         </div>
     );
 }
---
+++ src/component/SearchCharacter.tsx
@@ -0,0 +1,16 @@
+import {ChangeEvent} from "react";
+
+type SearchCharacterProps = {
+    searchText: (searchText: string) => void,
+}
+
+export default function SearchCharacter(props: SearchCharacterProps){
+
+    function handleOnChangeSearchText(event: ChangeEvent<HTMLInputElement>){
+        props.searchText(event.target.value);
+    }
+
+    return(
+        <input onChange={handleOnChangeSearchText}/>
+    );
+}
